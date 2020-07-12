b0a3c5a69c3cf31efdf91cf7ff1c98102cd0fda6
Talwinder Singh <nijjar.login@gmaio.com>
2020-07-12 02:54:17 -0400

    added call for devices page

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -6,6 +6,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import Paper from '@material-ui/core/Paper'
 
 const useStyles = makeStyles({
@@ -14,42 +15,79 @@ const useStyles = makeStyles({
     overflowX: 'auto',
   },
   table: {
-    minWidth: 700,
+    minWidth: 700
   },
+  textCenter: {
+    textAlign: 'center'
+  }
 });
 
 function Accounts() {
   const classes = useStyles();
 
-  const [accounts] = useState([{
-    id: 'Id1',
-    name: 'Name1'
-  }]);
-  
+  const [accounts, setAccounts] = useState([]);
+  const [isAccountsLoading, setIsAccountsLoading] = useState(true);
+  const [isAccountsLoadingError, setIsAccountsLoadingError] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(
+        `https://reqres.in/api/users`,
+        {
+          method: "GET"
+        }
+      )
+      .then(res => res.json())
+      .then(response => {
+        setAccounts(response.data);
+        setIsAccountsLoading(false);
+      })
+      .catch(error => {
+        setIsAccountsLoadingError(true);
+        setIsAccountsLoading(false);
+        console.log(error);
+      });
+    }, 2000);  
+  }, []);
+
   return (
     <div className="content">
-      <h1>Accounts</h1>
+      <h1 className = {classes.textCenter}>Accounts</h1>
 
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Id</TableCell>
-              <TableCell >Name</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {accounts.map(row => (
-              <TableRow key={row.id}>
-                <TableCell component="th" scope="row" >
-                  {row.id}
-                </TableCell>
-                <TableCell>{row.name}</TableCell>
+      {isAccountsLoading && 
+        <p className = {classes.textCenter}>
+          <LinearProgress color="secondary" />
+        </p>
+      }
+
+      {!isAccountsLoading && accounts.length > 0 &&
+        <Paper className={classes.root}>
+          <Table className={classes.table}  size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Id</TableCell>
+                <TableCell >First Name</TableCell>
+                <TableCell >Last Name</TableCell>
+                <TableCell >Email</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
+            </TableHead>
+            <TableBody>
+              {accounts.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row" >
+                    {row.id}
+                  </TableCell>
+                  <TableCell>{row.first_name}</TableCell>
+                  <TableCell>{row.last_name}</TableCell>
+                  <TableCell>{row.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      }
+
+    {isAccountsLoadingError && <p>Error Loading Accounts</p>}
     </div>
   );
 }
